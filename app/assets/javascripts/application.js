d2a5751cc7b36f13750816e2cbf227a5d5ba0be5
Richard Chou <konami99@hotmail.com>
2014-12-30 22:32:46 +1100

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,24 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+// require jquery
+// require jquery_ujs
 //= require turbolinks
-//= require_tree .
+// require_tree .
+//= require jquery.min
+//= require jquery-ui-1.10.3.custom.min
+//= require jquery.easing.1.3.min
+//= require html5
+//= require 'twitter/jquery.tweet'
+//= require jflickrfeed.min
+//= require jquery.inview.min
+//= require jquery.tipsy
+//= require tabs
+//= require jquery.flexslider
+//= require jquery.prettyPhoto
+//= require jquery.carouFredSel-6.2.1-packed
+//= require jquery.scrollTo
+//= require jquery.nav
+//= require tags
+//= require jquery.bxslider.min
+//= require custom
