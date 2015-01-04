fc3eae7a6c9e58fc7907cf0719efd6e6a46a4f9c
Richard Chou <konami99@hotmail.com>
2015-01-04 23:21:14 +1100

    index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
     @questions = Question.includes(:question_status).where('question_statuses.name = ?', 'Open').references(:question_status).order(created_at: :desc).limit(8)
-    s = @questions.size()
-    c = 'ff'
   end
 end
