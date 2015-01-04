480103f61e1e892460b160ab2f611d7866cba5f4
Richard Chou <konami99@hotmail.com>
2015-01-05 00:32:05 +1100

    index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @questions = Question.includes(:question_status).where('question_statuses.name = ?', 'Open').references(:question_status).order(created_at: :desc).limit(8)
+    @questions = Question.includes(:question_status, :user).where('question_statuses.name = ?', 'Open').references(:question_status).order(created_at: :desc).limit(8)
   end
 end
