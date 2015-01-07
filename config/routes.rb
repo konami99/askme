25adaa0c7918afc7493c64961bab9803c0cae18b
Richard Chou <konami99@hotmail.com>
2015-01-08 00:36:33 +1100

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  post 'signup' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
