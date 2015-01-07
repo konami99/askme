2ba034c2585532fdcbbdc06cd755eec2fd1a8f47
Richard Chou <konami99@hotmail.com>
2015-01-07 23:53:03 +1100

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   it 'is invalid without a password_digest'
 
-  it 'is invalid with a duplicate email address'
+  it 'is invalid with a duplicate email address' do
+    create(:user, email: 'test@gmail.com')
+    user = build(:user, email: 'test@gmail.com')
+    expect(user).to be_invalid
+  end
 
-  it 'is invalid with a duplicate username'
+  it 'is invalid with a duplicate username' do
+    create(:user, username: 'konami99')
+    user = build(:user, username: 'konami99')
+    expect(user).to be_invalid
+  end
 end
