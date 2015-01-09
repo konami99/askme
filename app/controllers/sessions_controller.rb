c1c46e97f3227ab8020cdef7a575a9acbcdf254c
Richard Chou <konami99@hotmail.com>
2015-01-09 22:44:21 +1100

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     #session[:username] = nil
-    #redirect_to root_path
+    redirect_to root_path
   end
 end
