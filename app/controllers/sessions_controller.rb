dee698f65d31ebdd0bbb7e6ee167b792d31f4a0a
Richard Chou <konami99@hotmail.com>
2015-01-10 21:59:08 +1100

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:])
+    user = User.find_by(username: params[:signin_username])
+    if user and user.authenticate(params[:signin_password])
+      session[:user_id] = user.id
+      session[;username] = user.username
+      redirect_to root_path
+    else
+
+    end
   end
 
   def destroy
