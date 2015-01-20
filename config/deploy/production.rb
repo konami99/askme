96efe5f9e2379d9f3611af746017192a9f2c9f7d
Richard Chou <konami99@hotmail.com>
2015-01-20 23:44:12 +1100

    remove sqlite3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{konami99@web323.webfaction.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
