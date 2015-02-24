58c867f33bde4aa2a0038ee1746a19b45a5bc13e
Richard Chou <konami99@hotmail.com>
2015-02-24 22:33:26 +1100

    like update

--- app/controllers/answer_attributes_controller.rb
+++ app/controllers/answer_attributes_controller.rb
@@ -2,20 +2,27 @@ class AnswerAttributesController < ApplicationController
   skip_before_action :authorize
 
   def update
-    aa = Answer.find(params[:answer_id]).answer_attribute
-    aa.like += 1
-    aa.save!
+    answer_attribute = Answer.find(params[:answer_id]).answer_attribute
+    answer_attribute.like += 1
+    answer_attribute.save!
     respond_to do |format|
       format.js {
         @answer_id = params[:answer_id]
-        @updatedCount = aa.like
+        @updatedCount = answer_attribute.like
       }
     end
 
   end
 
-  def show
-
-    s = 1
+  def destroy
+    answer_attribute = Answer.find(params[:answer_id]).answer_attribute
+    answer_attribute.like -= 1
+    answer_attribute.save!
+    respond_to do |format|
+      format.js {
+        @answer_id = params[:answer_id]
+        @updatedCount = answer_attribute.like
+      }
+    end
   end
 end
