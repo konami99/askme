c88d719c65485306832a2d960c5e4f124c841b9b
Richard Chou <konami99@hotmail.com>
2015-03-03 23:38:26 +1100

    cache on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_action :authorize, only: [:index]
 
   def index
-    @questions = Question.includes(:question_status, :user).where('question_statuses.name = ?', 'Open').references(:question_status).order(created_at: :desc).limit(8)
-    #@question = Question.order(:id)
+    @questions = Rails.cache.fetch('recent_questions', :expires_in => 5.minutes){
+      Question.includes(:question_status, :user).where('question_statuses.name = ?', 'Open').references(:question_status).order(created_at: :desc).limit(8)
+    }
   end
 end
