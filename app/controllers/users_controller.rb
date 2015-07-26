f23f1a152ac389a42a014764f0f16ac2ddf863ce
Richard Chou <konami99@hotmail.com>
2015-07-26 22:50:41 +1000

    layout update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :authorize, only: [:signin]
   def signin
-
+    render :layout => 'signin'
 
   end
 
