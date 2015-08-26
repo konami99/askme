26b2af07d9e719a33b964b7d8bfbb38f4f030b95
Richard Chou <konami99@hotmail.com>
2015-08-27 00:21:18 +1000

    question show

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   #get 'sessions/destroy'
 
   resources :answers do
-    resources :answer_attributes
+    resources :answer_attributes, only: [:update, :destroy]
   end
 
   resources :questions
