3a933f0831699603c35c28b82a01ef004aa2257e
Richard Chou <konami99@hotmail.com>
2015-09-10 23:21:49 +1000

    liked questions

--- app/models/liked_question.rb
+++ app/models/liked_question.rb
@@ -1,4 +1,16 @@
 class LikedQuestion < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  before_destroy :decrease_question_attribute_like
+  after_create :increase_question_attribute_like
+
+  private
+    def increase_question_attribute_like
+
+    end
+
+    def decrease_question_attribute_like
+
+    end
 end
