2157837155aca45d6c07780c2e0f42f22e40d845
Richard Chou <konami99@hotmail.com>
2015-09-13 23:49:46 +1000

    liked questions

--- app/models/liked_question.rb
+++ app/models/liked_question.rb
@@ -7,10 +7,13 @@ class LikedQuestion < ActiveRecord::Base
 
   private
     def increase_question_attribute_like
+      s = question.question_attribute.like.nil?
       question.question_attribute.like = (question.question_attribute.like.nil?)? 1 : question.question_attribute.like += 1
+      question.question_attribute.save
     end
 
     def decrease_question_attribute_like
       question.question_attribute.like = (question.question_attribute.like.nil?)? 1 : question.question_attribute.like -= 1
+      question.question_attribute.save
     end
 end
